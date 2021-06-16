42edbbb21cc1b51e908c812bdc8c1da30c90c619
julia <jmugure95@gmail.com>
2021-06-16 08:30:58 +0300

    angular signup form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -30,60 +30,8 @@ export class AppComponent implements OnInit{
     });
   }
 
-  getValue(): any{
-    // return this.signUpForm.get('medium') === 'Email' ? this.showPhone : !this.showEmail;
-  }
-
   onSubmit(): any{
     console.log(this.signUpForm);
-    // this.signUpForm.reset();
   }
 }
 
-
-// import {Component, OnInit} from '@angular/core';
-// import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
-//
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent implements OnInit{
-//   title = 'angularForm';
-//   genders = ['male', 'female'];
-//   mediums = ['Email', 'Phone'];
-//   signUpForm: FormGroup;
-//
-//   constructor(private formBuilder: FormBuilder) {
-//   }
-//
-//   ngOnInit(): void {
-//     this.signUpForm = new FormGroup({
-//       userData: new FormGroup({
-//         username: new FormControl(null, Validators.required),
-//         email: new FormControl(null, [Validators.required, Validators.email])
-//       }),
-//       gender: new FormControl(Validators.required),
-//       // hobbies: new FormArray([])
-//       hobbies: new FormArray([
-//         new FormControl('')])
-//     });
-//   }
-//
-//   get controls(): any{
-//     return (this.signUpForm.get('hobbies') as FormArray).controls;
-//   }
-//
-//   onAddHobby(): any{
-//     const control = new FormControl(null, Validators.required);
-//     (this.signUpForm.get('hobbies') as FormArray).push(control);
-//   }
-//
-//   onSubmit(): any{
-//     console.log(this.signUpForm);
-//     // this.signUpForm.reset();
-//   }
-//
-// }
-//
